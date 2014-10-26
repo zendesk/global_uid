1588766d9f9fc720f231336b61e6220aa8fc57c2
grosser <michael@grosser.it>
2014-10-25 20:02:14 -0700

    cleanup rakefile

--- lib/global_uid.rb
+++ lib/global_uid.rb
@@ -4,8 +4,6 @@ require "global_uid/migration_extension"
 require "global_uid/schema_dumper_extension"
 
 module GlobalUid
-  VERSION = "1.4.1"
-
   class NoServersAvailableException < Exception ; end
   class ConnectionTimeoutException < Exception ; end
   class TimeoutException < Exception ; end
