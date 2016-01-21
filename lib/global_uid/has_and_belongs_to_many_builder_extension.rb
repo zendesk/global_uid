1c3bbed081a86dbd9fb3829037e62e8722c24bbb
Pierre Schambacher <pschambacher@zendesk.com>
2016-01-21 16:26:47 +0000

    Replace alias_method_chain with 2 calls to alias_method

--- lib/global_uid/has_and_belongs_to_many_builder_extension.rb
+++ lib/global_uid/has_and_belongs_to_many_builder_extension.rb
@@ -2,7 +2,8 @@ module GlobalUid
   module HasAndBelongsToManyBuilderExtension
     def self.included(base)
       base.class_eval do
-        alias_method_chain :through_model, :inherit_global_uid_disabled_from_lhs
+        alias_method :through_model_without_inherit_global_uid_disabled_from_lhs, :through_model
+        alias_method :through_model, :through_model_with_inherit_global_uid_disabled_from_lhs
       end
     end
 
